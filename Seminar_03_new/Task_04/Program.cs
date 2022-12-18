90d22e7df3d5ac42987e2b7d3eae8c16690bbafb
SMV <kraft088@gmail.com>
2022-12-18 22:57:45 +1100

    Add Task 4

---
+++ Seminar_03_new/Task_04/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов 
+чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4
+*/
+
+// Console.Write("Введите число N:");
+// int finish = Convert.ToInt32(Console.ReadLine()); // Идем от 1 до N: [1;N]
+
+// int start = 1; // start -> finish
+
+// while (start <= finish)
+// {
+//     Console.Write(Math.Pow(start,2) + " ");
+//     start++; // start++ => start = start + 1
+//     // start += 1
+// }
+
+// 2 вариант:
+
+
