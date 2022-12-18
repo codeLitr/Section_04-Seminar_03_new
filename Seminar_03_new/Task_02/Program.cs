6df2b0e82ce72e8d2274505b3a1c6ce11e0e6eea
SMV <kraft088@gmail.com>
2022-12-18 22:53:13 +1100

    Add Task 2

---
+++ Seminar_03_new/Task_02/Program.cs
@@ -0,0 +1,11 @@
+﻿int quarterNumber = Convert.ToInt32(Console.ReadLine());
+
+if (quarterNumber == 1) Console.WriteLine("x > 0 && y > 0");
+
+else if (quarterNumber == 2) Console.WriteLine("x < 0 && y > 0");
+
+else if (quarterNumber == 3) Console.WriteLine("x < 0 && y < 0");
+
+else if (quarterNumber == 3) Console.WriteLine("x > 0 && y < 0");
+
+else Console.WriteLine("Ошибка в введении четверти");
