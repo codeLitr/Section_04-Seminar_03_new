2aabe16611763c6fe066a57876ba0933026e26ed
SMV <kraft088@gmail.com>
2022-12-22 23:50:45 +1100

    Add Home task 3

---
+++ HomeWork_03/HomeTask_03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число N:");
+int finish = Convert.ToInt32(Console.ReadLine()); 
+int start = 1; 
+
+while (start <= finish)
+{
+    Console.Write(Math.Pow(start,3) + " ");
+    start++; 
+}
