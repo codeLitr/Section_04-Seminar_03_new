bf212822c8d245583ffe2ec971685a2109fc66fe
SMV <kraft088@gmail.com>
2022-12-22 23:30:31 +1100

    Add Home task 2

---
+++ HomeWork_03/HomeTask_02/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между точками
+// в 3D пространстве.
+// A (3,6,8); B (2,1,-7) -> 15,84
+// A (7,-5,0); B (1,-1,9) -> 11,53
+
+Console.Write("Введите координату x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+//Console.WriteLine("Расстояние между 3 точками:" + x1 + "," + y1 + "," + z1 + "," + x2 + "," + y2 + "," + z2 + " = " + d );
+
+Console.WriteLine($"Расстояние между 3 точками: {x1},{y1},{z1}, {x2},{y2},{z2} = {Math.Round(d,2)}");
